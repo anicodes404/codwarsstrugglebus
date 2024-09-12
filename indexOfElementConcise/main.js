8efd08c6fcb974eabfdb3c78502de4662d371c6c
cplusinsanity <adnerdable@gmail.com>
2024-09-11 20:49:51 -0400

    Add comma problem and solution

--- indexOfElementConcise/main.js
+++ indexOfElementConcise/main.js
@@ -1,6 +1,4 @@
-function find(array, element){
-    for (let i = o; i < array.length; i++){
-        if (array[i] === element) return i; 
-    }
-    return "Not found"
-}
+// 
+
+
+const find = (a, x) => (x = a.indexOf(x)) > 0 ? 'Not found' : x
--- sumOfNumber/main.js
+++ sumOfNumber/main.js
@@ -5,3 +5,4 @@
 function sumMix(x){
     return x.map(a => +a).reduce((a, b) => a + b);
 }
+

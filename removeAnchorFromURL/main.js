3a6cc4de8ae23b1d21cc0113c68d9ad1bc52b0de
anicodes404 <adnerdable@gmail.com>
2025-02-12 22:47:19 -0500

    Added Name Array Capping problem and solution.

---
+++ removeAnchorFromURL/main.js
@@ -0,0 +1,6 @@
+const removeUrlAnchor = url => url.split('#')[0]
+
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
